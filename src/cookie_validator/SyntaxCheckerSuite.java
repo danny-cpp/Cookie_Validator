f03d47231e75e419ce8abb132fd196a5c485fbe6
Danh Nguyen <65102457+danny-cpp@users.noreply.github.com>
2020-09-28 22:06:52 -0400

    regex cp2

--- src/cookie_validator/SyntaxCheckerSuite.java
+++ src/cookie_validator/SyntaxCheckerSuite.java
@@ -7,22 +7,43 @@ interface IllegalCharacters {
     String cookie_illegal = "([^\\s,\\\\,\",;, \\,]*)";
 }
 
+interface DateAndTime {
+    String month = "(Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Sep|Oct|Nov|Dec)";
+    String wkday = "(Mon|Tue|Wed|Thu|Fri|Sat|Sun)";
+    String time = "([0-9]{2}:[0-9]{2}:[0-9]{2})";
+    String date1 = "[0-9]{2} " + month + " ([0-9]{4})";
+    String rfc1123_date = wkday + ", " + date1 + " " + time + " " + "GMT";
+}
+
+interface AcceptedValues extends DateAndTime {
+    String expires_av = "Expires=" + rfc1123_date;
+    String max_age_av = "(Max-Age=[1-9][0-9]*)";
+    String domain_av = "";
+    String path_av = "";
+    String secure_av = "";
+    String httponly_av = "";
+}
+
 /**
  * Internal cookie checker, not meant to be used outside the package.
  */
-public class SyntaxCheckerSuite implements IllegalCharacters {
+public class SyntaxCheckerSuite implements IllegalCharacters, AcceptedValues {
     protected static ArrayList<String> internal_cache = new ArrayList<>();
 
-    // protected static String cookie_av = expires_av + "|" + max_age_av  + "|" + domain_av+ "|" + path_av  + "|" + secure_av  + "|" + httponly_av;
+    protected static String cookie_av = expires_av + "|" + max_age_av  + "|" + domain_av + "|" +
+                                        path_av  + "|" + secure_av + "|" + httponly_av;
+
+
     protected static String cookie_value = "((\"" + cookie_illegal + "\")|" + cookie_illegal +")"; // Split into 2 cases: covered with quotes or not
     protected static String token = "([^" + separators + "]*)"; // Everything except separators
 
     protected static String cookie_pair = token + "=" + cookie_value;
 
-    protected static String set_cookie_string = cookie_pair; // + "*(\\; "+ cookie_av +")";
+    protected static String set_cookie_string = cookie_pair + "*(\\; "+ cookie_av +")";
     protected static String main_pattern = "^(Set-Cookie: )" + set_cookie_string;
 
     protected static boolean beginAndEnd(String s) {
+        internal_cache.clear();
 
         String pattern = "^(Set-Cookie: )([^(,),<,>,@,\\,,;,:,\\\\,\",\\/,\\[,\\],\\{,\\},\\?,=,\\s,\\t]*)";
 
@@ -37,6 +58,10 @@ public class SyntaxCheckerSuite implements IllegalCharacters {
             return false;
         }
 
+        for (String x: internal_cache) {
+            System.out.println(x);
+        }
+
         if (internal_cache.get(0).equals(s)) {
             return true;
         }
@@ -47,5 +72,6 @@ public class SyntaxCheckerSuite implements IllegalCharacters {
 
     public static void main(String[] args) {
         System.out.println(beginAndEnd("Set-Cookie: ns1=\"alss/0.foobar^\""));
+        System.out.println(beginAndEnd("Set-Cookie: lu=Rg3v; Expires=Wed, 19 Nov 2008 16:35:39 GMT"));
     }
 }
